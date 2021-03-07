8cb6b8dea6fd6566ce00c963d3c426b1e486aba1
MedAkrem <mohamedakrem.bargaoui@esprit.tn>
2021-03-07 20:47:19 +0100

    ajout

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include "fon.h"
+int main()
+{
+
+int choix;
+choix= menu( argc,  &argv)
+
+
+
+return EXIT_SUCCESS; // Fermeture du programme
+}
